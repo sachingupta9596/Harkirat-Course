799321fd2e7822b44690c14c6c6b4a113c7c847c
sachingupta9596 <22835626-sachingupta9596@users.noreply.replit.com>
2023-12-24 08:53:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+  res.send('Hello World');
+})
+
+app.get('/hello',(req,res)=>{
+  console.log(req);
+  res.send('Hello 2');
+})
+
+app.listen(3000);
